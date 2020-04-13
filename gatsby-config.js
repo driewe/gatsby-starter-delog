65c9dc67e89b0e18d1fd1df97d5263131f641c22
David Riewe <david.riewe@gmail.com>
2020-04-13 00:30:21 -0500

    added w31 key

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,8 +14,9 @@ module.exports = {
       title: `Hi, I'm David Riewe`,
       description: `Internet Marketing Consultant skilled in Amazon Web Services, WordPress, HTML, Javascript, PHP, Search Engine Optimization (SEO), web development, and e-commerce. Launched first website in 1997 and in 2005 started using wordpress. Most of the sites I build are for my own marketing goals to generate and resell leads. As of 2019 I have turned my focus to building niche business directories for clients.`,
     },
+
     /* W3Layouts domain verification key for contact forms https://my.w3layouts.com/Forms/ */
-    w3l_dom_key: `5e609f7a2d23fCF_Domain_verify` 
+    w3l_dom_key: `5e86d919dc044CF_Domain_verify` 
   },
   plugins: [
     {
